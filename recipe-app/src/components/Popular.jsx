65465a07d273365407b56e6ddd701a52bdda7bac
fgarrido <francisco.garrido@globalincubator.com>
2022-11-07 10:21:16 +0100

    Npm libraries and fetch api

--- recipe-app/src/components/Popular.jsx
+++ recipe-app/src/components/Popular.jsx
@@ -3,7 +3,7 @@ import { useEffect } from "react";
 
 function Popular() {
 
-    
+    const [popular, setPopular] = useState([]);
 
     useEffect( () => {
         getPopular();
@@ -13,11 +13,18 @@ function Popular() {
     const getPopular = async () => {
         const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`);
         const data = await api.json();
-        console.log(data);
+        setPopular(data.recipes);
     }
 
   return (
-    <div>Popular</div>
+    <div>
+      {popular.map((recipe) => {return (
+        <div key={recipe.id}>
+          <p>{recipe.title}</p>
+        </div>
+      );})}
+
+    </div>
     
   )
 }
